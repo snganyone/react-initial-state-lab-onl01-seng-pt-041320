03f1ebaedec1051fd659b883bd6267400a760fcf
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-12-17 15:14:47 -0800

    commit tree 4375445a3ff3280d4a1d29e9e99d8e3a5a1d27a1

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,29 @@
 // your Bomb code here!
+import React, { Component } from 'react';
+
+export default class Bomb extends Component {
+    constructor(props){
+        super();
+        this.state = {
+            secondsLeft: props.initialCount    
+        }
+    }
+
+    timer = () => {
+        if(this.state.secondsLeft > 0){
+            return(
+                <div>120 seconds left before I go boom!</div>
+            )
+        } else {
+            return(
+                <div>Boom!</div>
+            )
+        }
+    }
+
+    render(){
+        return (
+            <div>{this.timer()}</div>
+        )
+    }
+}
